f895fd101d5b6391aacfc8da7d0e433047ce3608
ebotic1 <ebotic1@etf.unsa.ba>
2023-07-02 15:31:50 +0200

    prvi lab

--- funkcije/izracunajPozNegTransformatora.m
+++ funkcije/izracunajPozNegTransformatora.m
@@ -2,7 +2,7 @@ function [P, N] = izracunajPozNegTransformatora(Zs1, Ysh1, n)
 
 p = [1/Zs1 + Ysh1/2, -1/Zs1; -1/Zs1, 1/Zs1 + Ysh1/2];
 
-P = [1, n; conj(n), n*n] * p;
-N = [1, conj(n); n, n*n] * p;
+P = [1, 0; 0, conj(n)] * p * [1 , 0; 0, n];
+N = conj([1, 0; 0, conj(n)])  * p * conj([1 , 0; 0, n]);
 end
 
---
+++ funkcije/spakujMatricu.m
@@ -0,0 +1,7 @@
+function [M1, M2, M3, M4] = spakujMatricu(mat)
+M1 = mat([1,2,3], 1:3);
+M2 = mat([1,2,3], 4:6);
+M3 = mat([4,5,6], 1:3);
+M4 = mat([4,5,6], 4:6);
+end
+
